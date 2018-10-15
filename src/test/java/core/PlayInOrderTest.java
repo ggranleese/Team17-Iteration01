b34b806da0225e944cc199b8e802977efe5f52f1
Lagawaga <kamronjahani@gmail.com>
2018-10-14 22:49:20 -0400

    Refactor: minor correction made to PlayInOrderTest

--- src/test/java/core/PlayInOrderTest.java
+++ src/test/java/core/PlayInOrderTest.java
@@ -6,12 +6,13 @@ public class PlayInOrderTest extends TestCase{
 	
 	public void testPlayIntOrder() {
 		int[] test = new int[4];
+		Player[] playerArray;
 		test[0] = 4;
 		test[1] = 1;
 		test[2] = 2;
 		test[3] = 3;
 		
-		PLayer[] playerArray = PlayInOrder(test);
+		playerArray = PlayInOrder(test);
 		
 		assertTrue(playerArray[3].playerNum == 1);
  	}

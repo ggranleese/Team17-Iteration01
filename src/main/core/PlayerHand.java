4c0666424460f49c4574212d0073d7d9b53040d8
Lagawaga <kamronjahani@gmail.com>
2018-10-15 13:48:16 -0400

    Code: implemented removeTile method

--- src/main/core/PlayerHand.java
+++ src/main/core/PlayerHand.java
@@ -11,9 +11,25 @@ private ArrayList<Tile> hand = new ArrayList<Tile>();
 		for(int i = 0; i<14; i++)
 			this.hand.add(pile.getPile().get(0));
 	}
+	
+	public void removeTile(int c, int v) {
+		
+		int n = this.hand.size();
+		
+		for(int i =0; i<n; i++) {
+			if(this.hand.get(i).getValue() == v && this.hand.get(i).getColour() == c) {
+				this.hand.remove(i);
+				break;
+			}
+		}
+	}
 
 	//GETTERS
 	public ArrayList<Tile> getHand() {
 		return this.hand;
+	}
+	
+	public int getSize() {
+		return this.hand.size();
 	}
 }

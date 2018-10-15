e6a3214c57120c46ef47044994a1e33ef690105b
austin-do <43225674+austin-do@users.noreply.github.com>
2018-10-14 23:32:40 -0400

    CODE: added code for RunOntoTableTest.

--- src/test/java/core/RunOntoTableTest.java
+++ src/test/java/core/RunOntoTableTest.java
@@ -2,18 +2,29 @@ package core;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
 import org.junit.Assert;
 import org.junit.Test;
 
-public class RunOntoTableTest {
+import junit.framework.TestCase;
+
+public class RunOntoTableTest extends TestCase{
 
 	@Test
 	public void test() {
-		Run run = new Run(new Tile(3,"G"), new Tile(4,"G"), new Tile(5,"G"));
-		Meld meld = new Meld(run); 
+		ArrayList<Tile> tiles = new ArrayList<Tile>();
+		tiles.add(new Tile(3,"G"));
+		tiles.add(new Tile(4,"G"));
+		tiles.add(new Tile(5,"G"));
+		
+		Run run = new Run(tiles);	
+		
 		Table table = new Table();
-		table.add(meld);
-		Assert.assertTrue(table.getMelds().get(0).type = 1);
+		table.add(run);
+		System.out.println(table.getMelds().size()); 
+		
+		Assert.assertTrue(table.getMelds().get(0).getTypeOfMeld() == 1);
 	}
 
 }

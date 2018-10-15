04506da70041ea0e225de380cbb6e4acf9d36700
Lagawaga <kamronjahani@gmail.com>
2018-10-15 14:13:49 -0400

    TEST: added PlayerHandToStringTest

---
+++ src/test/core/PlayerHandToStringTest.java
@@ -0,0 +1,19 @@
+package core;
+
+import java.util.ArrayList;
+
+import junit.framework.TestCase;
+
+public class PlayerHandToStringTest extends TestCase{
+
+	
+	public void testToString() {
+		PlayerHand playerHand = new PlayerHand();
+		
+		playerHand.addTile(new Tile(1,1));
+		playerHand.addTile(new Tile(1,2));
+		playerHand.addTile(new Tile(1,3));
+		
+		assertEquals(playerHand.toString(), "B1, B2, B3");
+	}
+}

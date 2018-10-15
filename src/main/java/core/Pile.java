ceebea283ff69f87994e4229c10233b955c7e62f
Lagawaga <kamronjahani@gmail.com>
2018-10-14 21:37:21 -0400

    implemented shuffle method to Pile class

--- src/main/java/core/Pile.java
+++ src/main/java/core/Pile.java
@@ -1,5 +1,35 @@
 package core;
 
+import java.util.ArrayList;
+import java.util.Random;
+
+
+
+
 public class Pile {
+	
+private ArrayList<Tile> pile;
+	
+	public Pile(){
+		this.pile = new ArrayList<Tile>();
+	}
+	
+	public Tile getTile(int index) {
+		return this.pile.get(index);
+	}
+	
+	public void shuffle() {
+		ArrayList<Tile> tmp = new ArrayList<Tile>();
+		Random rand = new Random();
+		int index = 0;
+		int size = this.pile.size();
+		for(int i=0; i < size; i++) {
+			index = rand.nextInt((this.pile.size()-1)+1);
+			tmp.add(this.pile.get(index));
+			this.pile.remove(index);
+		}
+		
+		this.pile = tmp;
+	}
 
 }

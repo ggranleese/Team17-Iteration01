5baceb0cd868cfa4038d6e38416be28be80b5bdc
austin-do <43225674+austin-do@users.noreply.github.com>
2018-10-15 21:29:43 -0400

    BUG: recommiting old InvalidRunsTest

---
+++ src/test/core/InvalidRunsTest.java
@@ -0,0 +1,14 @@
+package core;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class InvalidRunsTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}

4cb4c59f0ef0d074ee973977bab60dff11afd990
austin-do <43225674+austin-do@users.noreply.github.com>
2018-10-19 11:25:15 -0400

    REFACTOR: replaced PlayerHand with Player, AI extends Player.

--- src/test/core/ThirtyPointsTest.java
+++ src/test/core/ThirtyPointsTest.java
@@ -2,24 +2,25 @@ package core;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
 import org.junit.Test;
 
 public class ThirtyPointsTest {
 
 	@Test
 	public void test() {
-		Player player1 = new Player ();
+		Player player1 = new Player();
 		
 		assertTrue(!player1.getStatus());
 		
-		Player player2 = new Player ();
-		PlayerHand hand = player2.getPlayersHand();
-		hand.addTile(new Tile(1,6));
+		Player player2 = new Player();
+		ArrayList<Tile> hand = player2.getHand();
 		hand.addTile(new Tile(1,7));
 		hand.addTile(new Tile(1,8));
 		hand.addTile(new Tile(1,9));
 		
-		Run run = new Run(hand.getHand());
+		Run run = new Run(player2.getHand());
 		hand.playMeld(run);
 		
 		assertTrue(player2.getStatus());

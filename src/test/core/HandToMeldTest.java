239fc14f26ef67a2a4156f47acd89a2b5d26584c
austin-do <43225674+austin-do@users.noreply.github.com>
2018-10-19 18:33:03 -0400

    TEST: added HandToMeldTest test case.

---
+++ src/test/core/HandToMeldTest.java
@@ -0,0 +1,32 @@
+package core;
+
+import java.util.ArrayList;
+
+import junit.framework.TestCase;
+
+public class HandToMeldTest extends TestCase{
+	public void test() {
+		Table table = new Table();
+		Player player = new Player();
+		
+		table.registerObserver(player);
+		table.notifyObservers();
+		
+		ArrayList<Tile> tiles = new ArrayList<Tile>();
+		tiles.add(new Tile(1,10));
+		tiles.add(new Tile(2,10));
+		tiles.add(new Tile(3,10));
+		
+		Run run = new Run(tiles);
+		
+		player.playMeld(run);
+		
+		int initial = table.getMelds().get(0).getTiles().size();
+		
+		player.addToMeld(0, table.getMelds().get(0));
+		
+		assertTrue(initial < table.getMelds().get(0).getTiles().size());
+		
+	}
+
+}

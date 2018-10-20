edbe8d4762bc65daca9fc1e492da28d0d42fd515
austin-do <43225674+austin-do@users.noreply.github.com>
2018-10-19 22:50:38 -0400

    TEST: added MultipleHandToMeldTest test case.

---
+++ src/test/core/MultipleHandToMeldTest.java
@@ -0,0 +1,39 @@
+package core;
+
+import java.util.ArrayList;
+
+import junit.framework.TestCase;
+
+public class MultipleHandToMeldTest extends TestCase{
+
+	public void test() {
+		Table table = new Table();
+		Player player = new Player();
+		
+		table.registerObserver(player);
+		table.notifyObservers();
+		table.getPile().populate();
+		table.getPile().shuffle();
+		
+		ArrayList<Tile> tiles = new ArrayList<Tile>();
+		tiles.add(new Tile(1,10));
+		tiles.add(new Tile(2,10));
+		tiles.add(new Tile(3,10));
+		
+		Run run = new Run(tiles);
+		
+		player.playMeld(run);
+		
+		player.pushToTable(table);
+		
+		int initial = table.getMelds().get(0).getTiles().size();
+	
+		player.drawHand();
+		
+		player.addToMeld();
+		player.pushToTable(table);
+		
+		assertTrue(initial + 1 < table.getMelds().get(0).getTiles().size());
+	}
+
+}

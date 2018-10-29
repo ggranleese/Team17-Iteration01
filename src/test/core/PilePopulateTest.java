61c0fdb1639f51120836e6039519b6d420c12676
Lagawaga <kamronjahani@gmail.com>
2018-10-29 15:43:05 -0400

    Refactor: populate was only creating 78 tiles, fixed method to create 104

--- src/test/core/PilePopulateTest.java
+++ src/test/core/PilePopulateTest.java
@@ -7,6 +7,7 @@ public class PilePopulateTest extends TestCase {
 	public void testPopulatePile() {
 		Pile pile = new Pile();
 		pile.populate();
+		System.out.println(pile.getPile().size());
 		
 		assertTrue(pile.getPile().size() != 0);
 		assertTrue(pile.getPile().size() == 104);

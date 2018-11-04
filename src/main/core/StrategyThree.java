246982fca9935a57292cb04af1602f2687b9100e
Lagawaga <kamronjahani@gmail.com>
2018-11-04 15:30:45 -0500

    TEST: added tests to assure strategy 3 would follow all of its requirements

--- src/main/core/StrategyThree.java
+++ src/main/core/StrategyThree.java
@@ -1,5 +1,30 @@
 package core;
 
-public class StrategyThree implements IStrategy {
+import java.util.ArrayList;
 
+public class StrategyThree implements IStrategy {
+	
+	public ArrayList<Meld> play(ArrayList<Tile> hand){
+		return checkPlays(hand);
+	}
+	
+	public ArrayList<Meld> checkPlays(ArrayList<Tile> hand){
+		ArrayList<Meld> tmp;
+		tmp.add(checkRun(hand));
+		tmp.add(checkSet(hand));
+		
+		return tmp;
+	
+	}
+	
+	public int  checkTotalPoints(ArrayList<Meld> melds) {
+		int counter = 0;
+		for(Meld m : melds) {
+			for (Tile t : m.getTiles()) {
+				counter += t.getValue();
+			}
+		}
+		
+		return counter;
+	}
 }
